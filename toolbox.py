6e02590f690bff420d4a64aa6d2eba7209015341
gabeweng <gabeweng@gmail.com>
2024-04-11 21:24:52 -0400

    itertools

--- toolbox.py
+++ toolbox.py
@@ -4,3 +4,24 @@ arr = input().split() #array of strings
 arr = list(map(int, input().split())) #array of integers
 
 
+#itertools
+from itertools import permutations
+
+'''
+
+
+product('ABCD', repeat=2) #exactly like nested for loop
+
+AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
+
+permutations('ABCD', 2) #like above but no repeating elemetnts
+
+AB AC AD BA BC BD CA CB CD DA DB DC
+
+combinations('ABCD', 2) #subarrays (essentailly there will be AB but no BA)
+
+AB AC AD BC BD CD
+
+combinations_with_replacement('ABCD', 2) #subarrays but repeat for some reason?
+
+AA AB AC AD BB BC BD CC CD DD'''
